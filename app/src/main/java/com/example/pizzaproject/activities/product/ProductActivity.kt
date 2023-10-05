908037aac943f62650e2574c0f18ce4c413b45e0
rotoyouoi <rotoyouoi@yandex.ru>
2023-10-05 10:07:25 +0500

    Отлично работа идет полным ходом пока что все хорошо дай бог анна владимировна случайно апи не выключит.

--- app/src/main/java/com/example/pizzaproject/activities/product/ProductActivity.kt
+++ app/src/main/java/com/example/pizzaproject/activities/product/ProductActivity.kt
@@ -2,22 +2,29 @@ package com.example.pizzaproject.activities.product
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.pizzaproject.R
 import com.example.pizzaproject.activities.main.MainActivity
+import com.example.pizzaproject.adapters.CategoriesAdapter
+import com.example.pizzaproject.adapters.ProductDetailAdapter
 import com.example.pizzaproject.databinding.ActivityProductBinding
 import com.example.pizzaproject.datasource.ServiceBuilder
 import com.example.pizzaproject.interfaces.APIServiceInterface
+import com.example.pizzaproject.model.category.ApiResponseCategory
+import com.example.pizzaproject.model.product.ApiResponseProduct
 import com.example.pizzaproject.model.product.Product
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class ProductActivity : AppCompatActivity() {
 
      private lateinit var binding: ActivityProductBinding
 
-     val productList = ArrayList<Product>()
+     var productList = ArrayList<Product>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +39,23 @@ class ProductActivity : AppCompatActivity() {
     }
 
     private fun getProductById(id: Int){
-
+        val retrofit = ServiceBuilder.buildService(APIServiceInterface::class.java)
+        retrofit.getProductsById(id).enqueue(object: Callback<ApiResponseProduct>{
+            override fun onResponse(call: Call<ApiResponseProduct>, response: Response<ApiResponseProduct>){
+                try{
+                    val responseBody = response.body()!!
+                    productList = responseBody.data
+                    val adapter = ProductDetailAdapter(productList)
+                    binding.productsRecycler.adapter = adapter
+                }
+                catch (ex:java.lang.Exception){
+                    ex.printStackTrace()
+                }
+            }
+            override fun onFailure(call: Call<ApiResponseProduct>, t: Throwable) {
+                Log.e("Failed", "Api Failed" + t.message)
+            }
+        })
     }
 
 
--- app/src/main/java/com/example/pizzaproject/adapters/ProductDetailAdapter.kt
+++ app/src/main/java/com/example/pizzaproject/adapters/ProductDetailAdapter.kt
@@ -11,41 +11,40 @@ import com.example.pizzaproject.R
 import com.example.pizzaproject.model.product.Product
 import com.squareup.picasso.Picasso
 
-class ProductsAdapter(
-    private val list: List<Product>,
-    private val onClick: (id: Int) -> Unit
-): RecyclerView.Adapter<ProductsAdapter.ViewHolder>()  {
+class ProductDetailAdapter(
+    private val list: List<Product>
+    ) : RecyclerView.Adapter<ProductDetailAdapter.ViewHolder>()
+{
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
         val image : ImageView = itemView.findViewById(R.id.pizzaImage)
         val title : TextView = itemView.findViewById(R.id.pizzaTitle)
         val description : TextView = itemView.findViewById(R.id.pizzaDescription)
-        val priceButton : Button = itemView.findViewById(R.id.priceAndInfoButton)
-        init {
-            priceButton.setOnClickListener{
-                onClick(list[adapterPosition].idProduct)
-            }
-        }
+
+
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_list,parent,false)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ProductDetailAdapter.ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_detail,parent,false)
         return ViewHolder(view)
 
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+    override fun onBindViewHolder(holder: ProductDetailAdapter.ViewHolder, position: Int) {
         val product = list[position]
         holder.title.text = product.nameProduct
         holder.description.text = product.description
-        holder.priceButton.text = "от " +  product.price.toString() + "р "
         Picasso.get().load( "http://172.30.44.151:8090/images/product/"+product.image).into(holder.image)
-
-
     }
+
     override fun getItemCount(): Int {
-      return list.size
+        return list.size
     }
+
+
 }
