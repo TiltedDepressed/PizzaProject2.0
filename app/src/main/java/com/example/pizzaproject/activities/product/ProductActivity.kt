20094a159a09242a74381818c87f4498275e1852
rotoyouoi <rotoyouoi@yandex.ru>
2023-10-09 13:34:09 +0500

    .

--- app/src/main/java/com/example/pizzaproject/activities/product/ProductActivity.kt
+++ app/src/main/java/com/example/pizzaproject/activities/product/ProductActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.isGone
 import com.example.pizzaproject.R
 import com.example.pizzaproject.activities.main.MainActivity
 import com.example.pizzaproject.adapters.CategoriesAdapter
@@ -31,6 +34,8 @@ class ProductActivity : AppCompatActivity() {
 
      var productList = ArrayList<Product>()
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProductBinding.inflate(layoutInflater)
@@ -53,11 +58,42 @@ class ProductActivity : AppCompatActivity() {
         retrofit.getProductsById(id).enqueue(object: Callback<ApiResponseProduct>{
             override fun onResponse(call: Call<ApiResponseProduct>, response: Response<ApiResponseProduct>) {
                 try{
+                       /* size buttons container */
+                    val sizeButtonsContainer : LinearLayout = findViewById(R.id.sizeButtonsContainer)
+                    val sizeButtonSmall : Button = findViewById(R.id.sizeButtonSmall)
+                    val sizeButtonMedium : Button = findViewById(R.id.sizeButtonMedium)
+                    val sizeButtonBig : Button = findViewById(R.id.sizeButtonBig)
+
+                        /* type buttons container  */
+                    val typeButtonsContainer : LinearLayout = findViewById(R.id.typeButtonsContainer)
+                    val typeButtonTraditional : Button = findViewById(R.id.typeButtonTraditional)
+                    val typeButtonSlim : Button = findViewById(R.id.typeButtonSlim)
+
+
+                    val addPizzaText : TextView = findViewById(R.id.AddPizzaText)
+                    val pizzaSize : TextView = findViewById(R.id.pizzaSize)
+
+
+
                     val responseBody = response.body()!!
                     productList = responseBody.data
                     Picasso.get().load( "http:/172.30.44.151:8090/images/product/"+productList[0].image).into(binding.pizzaImage)
                     binding.pizzaTitle.text=productList[0].nameProduct
                     binding.pizzaDescription.text = productList[0].description
+
+                    if(productList[0].categoryId == 1){
+
+                        pizzaSize.text = "Выберете размер для вашей пиццы."
+
+
+                    }
+
+                    if (productList[0].categoryId != 1 ){
+                        sizeButtonsContainer.visibility = View.GONE
+                        typeButtonsContainer.visibility = View.GONE
+                        addPizzaText.visibility = View.GONE
+                    }
+
 
                 }
                 catch (ex:java.lang.Exception){
